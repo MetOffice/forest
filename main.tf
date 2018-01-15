d99760e557b329edc5a48aea474875e64a8a67e7
Stephen Haddad <stevehadd@yahoo.com>
2018-01-15 17:25:19 +0000

    SEA #46 - upgraded to larger EC2 instance type.

--- main.tf
+++ main.tf
@@ -8,10 +8,13 @@ data "template_file" "bootstrap" {
 
 resource "aws_instance" "bokeh_server" {
   ami                   = "ami-e7d6c983"
-  instance_type         = "t2.medium"	
+  instance_type         = "t2.xlarge"	
   key_name              = "kubernetes.cluster.k8s.informaticslab.co.uk-be:87:08:3a:ea:a2:9e:7e:be:c1:97:2a:42:9b:8a:05"
   user_data             = "${data.template_file.bootstrap.rendered}"
   # iam_instance_profile  = "seasia-bokeh-on-ec2"
+  root_block_device {
+       volume_size = 80
+   }
   tags {
     Name = "model_evaluation_tool_server",
     EndOfLife = "2018-03-31",
@@ -23,6 +26,7 @@ resource "aws_instance" "bokeh_server" {
   }
   security_groups        = ["default", "${aws_security_group.server.name}"]
 }
+
 resource "aws_security_group" "server" {
   name = "model_evaluation_tool"
 }
