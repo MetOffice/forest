973b2429bbf0ccd0c6dd8730e0b8f76736546bbe
Stephen Haddad <stevehadd@yahoo.com>
2018-02-05 16:17:32 +0000

    MET #20: Fixed further issues with deployment setup of branch. Deployment is now working correctly. Setting will obviously have to be changed to main settings when merging.

--- deployment/run.sh
+++ deployment/run.sh
@@ -5,7 +5,7 @@ pysssix /s3 >/dev/null 2>&1 &
 export BOKEH_APPS_DIR=/opt/bokeh_apps
 if [ -z "$PUBLIC_IP" ]
 then
-    bokeh serve --port 8888 $BOKEH_APPS/*
+    bokeh serve --port 8888 ${BOKEH_APPS_DIR}/*
 else
-    bokeh serve --allow-websocket-origin $PUBLIC_IP:8888 --port 8888 $BOKEH_APPS/*
+    bokeh serve --allow-websocket-origin $PUBLIC_IP:8888 --port 8888 ${BOKEH_APPS_DIR}/*
 fi
