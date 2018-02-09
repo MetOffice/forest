c8fcab71260b71503e20383cde3479702ab8b520
Theo McCaie <tam203@gmail.com>
2018-02-09 14:37:35 +0000

    add goofys

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
-pysssix /s3 >/dev/null 2>&1 &
+# TODO this should be in a loop
+mkdir -p /s3/mogreps-uk
+goofys mogreps-uk /s3/mogreps-uk
+
+mkdir -p /s3/mogreps-g
+goofys mogreps-g /s3/mogreps-g
+
+mkdir -p /s3/graeme-misc-london
+goofys graeme-misc-london /s3/graeme-misc-london
+
+mkdir -p /s3/stephen-sea-public-london
+goofys stephen-sea-public-london /s3/stephen-sea-public-london
 
 export BOKEH_APPS_DIR=/opt/bokeh_apps
 if [ -z "$PUBLIC_IP" ]
