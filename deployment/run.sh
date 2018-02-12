e52955df854d0f6751d561d388f4ba5d54a3fef8
Stephen Haddad <stevehadd@yahoo.com>
2018-02-12 11:46:54 +0000

    MET #11: Update to deployment to support mounting of s3 buckets using goofys.

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-# TODO this should be in a loop
-
-mkdir -p ~/s3/stephen-sea-public-london
-goofys stephen-sea-public-london ~/s3/stephen-sea-public-london
+ln -s /opt/s3 ~/s3
 
 export BOKEH_APPS_DIR=/opt/bokeh_apps
+
 if [ -z "$PUBLIC_IP" ]
 then
     bokeh serve --port 8888 ${BOKEH_APPS_DIR}/*
