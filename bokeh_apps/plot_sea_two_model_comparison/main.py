cc3bf20277f226da55d5d6060e21c3209b83e504
Stephen Haddad <stephen.haddad@metoffice.gov.uk>
2018-02-13 16:21:24 +0000

    MET #11: Set initial variable back to precipitation.

--- bokeh_apps/plot_sea_two_model_comparison/main.py
+++ bokeh_apps/plot_sea_two_model_comparison/main.py
@@ -146,7 +146,7 @@ def main(bokeh_id):
     plot_opts = forest.util.create_colour_opts(plot_names)
 
     init_data_time = 4
-    init_var = plot_names[3] #blank
+    init_var = plot_names[0] #blank
     init_region = 'se_asia'
     init_model_left = forest.data.N1280_GA6_KEY # KM4P4_RA1T_KEY
     init_model_right = forest.data.KM4P4_RA1T_KEY # N1280_GA6_KEY
