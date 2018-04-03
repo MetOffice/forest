48a451a0643a8ad5d2c99298b13d065fd095cbae
Stephen Haddad <stevehadd@yahoo.com>
2018-04-03 16:59:37 +0100

    MET #17: reverted to using port 80 for now.

--- deployment/run.sh
+++ deployment/run.sh
@@ -6,7 +6,7 @@ export BOKEH_APPS_DIR=/opt/bokeh_apps
 
 if [ -z "$PUBLIC_IP" ]
 then
-    bokeh serve --port=443 --num-procs=0 ${BOKEH_APPS_DIR}/*
+    bokeh serve --port=80 --num-procs=0 ${BOKEH_APPS_DIR}/*
 else
-    bokeh serve --allow-websocket-origin $PUBLIC_IP:443 --port=443 --num-procs=0 ${BOKEH_APPS_DIR}/*
+    bokeh serve --allow-websocket-origin $PUBLIC_IP:80 --port=80 --num-procs=0 ${BOKEH_APPS_DIR}/*
 fi
