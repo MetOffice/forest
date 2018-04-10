2d42b5d282880427626db82094553ba3457e602b
Stephen Haddad <stevehadd@yahoo.com>
2018-04-10 14:12:11 +0100

    forest #17: typo in websockets bokeh option.

--- forest_lib/run_forest_server.sh
+++ forest_lib/run_forest_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export S3_ROOT=$1
-bokeh serve --allow-websocket-origin forestkube.informaicslab.co.uk:$2 --port=$2 $3/*
+bokeh serve --allow-websocket-origin forestkube.informaticslab.co.uk:$2 --port=$2 $3/*
