b1445d1a47b5ea6fa6b35483661cdb84b4079f9a
Stephen Haddad <stevehadd@yahoo.com>
2018-04-10 15:35:15 +0100

    forest #17: trying again to use forest.informaticslab.co.uk URL

--- forest_lib/run_forest_server.sh
+++ forest_lib/run_forest_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export S3_ROOT=$1
-bokeh serve --allow-websocket-origin forestkube.informaticslab.co.uk --port=$2 $3/*
+bokeh serve --allow-websocket-origin ${FOREST_URL} --port=$2 $3/*
