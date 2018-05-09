9859bccfcad026426a4a323293558f9c64103b85
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-05-09 17:01:36 +0100

    improved unit test to fake ModelGpmControl construction

--- bokeh_apps/plot_sea_model_and_gpm_mpl/test_model_gpm_control.py
+++ bokeh_apps/plot_sea_model_and_gpm_mpl/test_model_gpm_control.py
@@ -3,18 +3,30 @@ import model_gpm_control
 
 
 class TestModelGpmControl(unittest.TestCase):
+    def setUp(self):
+        self.init_fcast_time = "20180101"
+
     def test_can_be_constructed(self):
         init_var = ""
-        datasets = []
+        mock_gpm_dataset = unittest.mock.Mock()
+        mock_gpm_dataset.get_times.return_value = [
+            self.init_fcast_time
+        ]
+        datasets = {self.init_fcast_time: {
+            "gpm_imerg_early": {
+                "data": mock_gpm_dataset
+            }
+        }}
         init_time_ix = 0
-        init_fcast_time = ""
+        init_fcast_time = self.init_fcast_time
         plot_list = []
         bokeh_img_list = []
-        stats_list = []
-        model_gpm_control.ModelGpmControl(init_var,
-                                          datasets,
-                                          init_time_ix,
-                                          init_fcast_time,
-                                          plot_list,
-                                          bokeh_img_list,
-                                          stats_list)
+        stats_list = [None, None]
+        with unittest.mock.patch("model_gpm_control.bokeh") as bokeh:
+            model_gpm_control.ModelGpmControl(init_var,
+                                              datasets,
+                                              init_time_ix,
+                                              init_fcast_time,
+                                              plot_list,
+                                              bokeh_img_list,
+                                              stats_list)
