0319419cd917ff0a9b1e14b210a0f198de9be92e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-05-09 15:28:10 +0100

    fixed unit test to understand dictionary structure

--- bokeh_apps/plot_sea_model_and_gpm_mpl/test_main.py
+++ bokeh_apps/plot_sea_model_and_gpm_mpl/test_main.py
@@ -6,6 +6,7 @@ import main
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.init_fcast_time = "20180101"
+        self.gpm_dataset = "GpmDataset"
 
     def test_main_can_be_called(self):
         with unittest.mock.patch("main.bokeh") as bokeh:
@@ -14,12 +15,15 @@ class TestMain(unittest.TestCase):
     @unittest.mock.patch("main.forest.plot.ForestPlot")
     @unittest.mock.patch("main.forest.data.get_available_times")
     @unittest.mock.patch("main.forest.data.get_available_datasets")
+    @unittest.mock.patch("main.model_gpm_data")
     @unittest.mock.patch("main.model_gpm_control")
     def test_main_calls_modelgpmcontrol(self,
                                         model_gpm_control,
+                                        model_gpm_data,
                                         get_available_datasets,
                                         get_available_times,
                                         ForestPlot):
+        model_gpm_data.GpmDataset.return_value = self.gpm_dataset
         get_available_datasets.return_value = self.init_fcast_time, {self.init_fcast_time: {}}
         get_available_times.return_value = [None, None, None, None, None]
         with unittest.mock.patch("main.bokeh") as bokeh:
@@ -28,12 +32,12 @@ class TestMain(unittest.TestCase):
             expect = {
                self.init_fcast_time: {
                    "gpm_imerg_early": {
-                       "data": None,
+                       "data": self.gpm_dataset,
                        "data_type_name": "GPM IMERG Early",
                        "gpm_type": "early"
                    },
                    "gpm_imerg_late": {
-                       "data": None,
+                       "data": self.gpm_dataset,
                        "data_type_name": "GPM IMERG Late",
                        "gpm_type": "late"
                    },
