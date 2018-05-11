120d477d52f25fadb074834d7588f835425808ff
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-05-11 10:50:13 +0100

    first steps towards constructing a ForestPlot

--- bokeh_apps/plot_sea_model_and_gpm_mpl/test_forest_plot.py
+++ bokeh_apps/plot_sea_model_and_gpm_mpl/test_forest_plot.py
@@ -4,4 +4,32 @@ import forest
 
 class TestForestPlot(unittest.TestCase):
     def test_can_be_constructed(self):
-        pass
+        """the minimal information needed to construct a ForestPlot"""
+        dataset = {
+            "current_config": {
+                "data_type_name": None
+            }
+        }
+        model_run_time = None
+        po1 = None
+        figname = None
+        plot_var = None
+        conf1 = "current_config"
+        reg1 = None
+        rd1 = None
+        unit_dict = None
+        unit_dict_display = None
+        app_path = None
+        init_time = None
+        forest.plot.ForestPlot(dataset,
+                               model_run_time,
+                               po1,
+                               figname,
+                               plot_var,
+                               conf1,
+                               reg1,
+                               rd1,
+                               unit_dict,
+                               unit_dict_display,
+                               app_path,
+                               init_time)
