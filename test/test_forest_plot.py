52f53065456663307feff28a29ee3057d3e03caf
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-02 14:14:42 +0100

    began to cover ForestPlot with unit tests to refactor design

--- test/test_forest_plot.py
+++ test/test_forest_plot.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock
 import forest
 
 
@@ -83,3 +84,68 @@ class TestForestPlot(unittest.TestCase):
             mock_plot.assert_called_once_with()
             self.assertEqual(forest_plot.current_config, "new_config")
             self.assertEqual(forest_plot.plot_description, "Label")
+
+    @unittest.skip("exploring forest architecture")
+    @unittest.mock.patch("forest.plot.matplotlib")
+    @unittest.mock.patch("forest.plot.bokeh")
+    def test_forest_plot_calls_bokeh_plotting_figure(self,
+                                                     bokeh,
+                                                     matplotlib):
+        """ForestPlot constructs its own bokeh.Figure instance
+
+        .. note:: Every time create_plot() is called a call
+                  to create_bokeh_img_plot_from_fig() inside
+                  which bokeh.plotting.figure() is called
+
+        .. warn:: ForestPlot does not initialise a reference
+                  to self.current_figure that is done by
+                  self.create_matplotlib_fig()
+
+        .. note:: ForestPlot.create_matplotlib_fig() expects
+                  self.current_var to be a key in self.plot_funcs
+
+        .. note:: ForestPlot.get_image_array_from_figure() takes
+                  a matplotlib.Figure instance
+        """
+        fixture = forest.plot.ForestPlot(*self.generic_args())
+        fixture.plot_funcs["plot_variable"] = unittest.mock.Mock()
+        fixture.create_matplotlib_fig()
+        fixture.create_bokeh_img_plot_from_fig()
+        bokeh.plotting.figure.assert_called_once_with()
+
+    def test_forest_plot_should_accept_figure(self):
+        """To open forest to allow generic layouts"""
+        forest.plot.ForestPlot(*self.generic_args())
+
+    def generic_args(self):
+        """Helper to construct ForestPlot"""
+        dataset = {
+            "current_config": {
+                "data_type_name": None
+            }
+        }
+        model_run_time = None
+        po1 = None
+        figname = None
+        plot_var = "plot_variable"
+        conf1 = "current_config"
+        reg1 = "current_region"
+        rd1 = {
+            "current_region": None
+        }
+        unit_dict = None
+        unit_dict_display = None
+        app_path = None
+        init_time = None
+        return (dataset,
+                model_run_time,
+                po1,
+                figname,
+                plot_var,
+                conf1,
+                reg1,
+                rd1,
+                unit_dict,
+                unit_dict_display,
+                app_path,
+                init_time)
