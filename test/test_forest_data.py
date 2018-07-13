4a066c9191ff143029cada0c9075c3089a2a3885
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-13 15:32:32 +0100

    added unit test to understand ForestDataset class

--- test/test_forest_data.py
+++ test/test_forest_data.py
@@ -2,6 +2,20 @@ import unittest
 import forest
 import numpy as np
 
+class TestForestDataset(unittest.TestCase):
+    def test_can_be_constructed(self):
+        forest.data.ForestDataset(*self.generic_args())
+
+    def generic_args(self):
+        config = None
+        file_name = None
+        s3_base = None
+        s3_base_local = None
+        use_s3_mount = None
+        base_local_path = None
+        do_download = None
+        var_lookup = None
+        return (config, file_name, s3_base, s3_base_local, use_s3_mount, base_local_path, do_download, var_lookup)
 
 class TestGetAvailableTimes(unittest.TestCase):
     def test_get_available_times(self):
