873423f5953be3e4fdfdbb6a8c1dc12ac776b414
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-07-18 15:48:26 +0100

    hacked figure constructor to switch off HoverTool associated with Slider

--- bokeh_apps/plot_sea_two_model_comparison/main.py
+++ bokeh_apps/plot_sea_two_model_comparison/main.py
@@ -123,7 +123,8 @@ def main(bokeh_id):
 
     user_interface = "single-plot"
     if user_interface == "single-plot":
-        bokeh_figure = bokeh.plotting.figure(toolbar_location="above")
+        bokeh_figure = bokeh.plotting.figure(toolbar_location="above",
+                                             active_inspect=None)
 
     # Set up plots
     plot_obj_left = forest.plot.ForestPlot(datasets[init_fcast_time],
