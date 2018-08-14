5b9c6e35506fa33c444c26d597fa02888fec8347
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-08-14 13:58:50 +0100

    removed Toggle class as feature provided by global_alpha property

--- forest_lib/forest/image.py
+++ forest_lib/forest/image.py
@@ -27,37 +27,6 @@ position.
 >>> slider = forest.image.Slider(left_images, right_images)
 >>> slider.add_figure(figure)
 
-Toggle
-------
-
-A :class:`.Toggle` switches between left/right images to give
-a quick overview of differences in two images. The effect is achieved
-by caching and modifying each images alpha values
-
->>> toggle = forest.image.Toggle(left_images, right_images)
->>> toggle.show_left()  # Set left image to visible
->>> def callback(attr, old, new):
-...     if new == 0:
-...         toggle.show_left()
-...     else:
-...         toggle.show_right()
->>> buttons = bokeh.models.widgets.RadioButtonGroup(
-...     labels=["Left", "Right"],
-...     active=0
-... )
->>> buttons.on_change("active", callback)
-
-A :class:`Toggle` has no knowledge of bokeh widgets
-or layouts, it simply exposes methods :func:`Toggle.show_left` and
-:func:`Toggle.show_right` so that the user to craft their own user interface
-
-In the example shown above, a toggle is constructed using
-the data sources of two bokeh RGBA images. A callback and
-radio button group are then created to allow the user to
-hide/show the appropriate images. Since the active button
-is Left the toggle `show_left()` method has been called
-to get the app into a consistent state
-
 Application programming interface (API)
 ---------------------------------------
 
@@ -102,72 +71,6 @@ class CachedRGBA(object):
             self.source.data["_alpha"] = get_alpha(self.source)
             for callback in self._callbacks:
                 callback()
-
-# TODO: We can get rid of this?
-
-
-class Toggle(object):
-    """Controls alpha values of bokeh RGBA ColumnDataSources
-
-    Similar design to :class:`.Slider` but with wholesale replacement
-    of image alpha values
-
-    :param left_images: ColumnDataSource or GlyphRenderer used to
-                        define RGBA images when toggle is set to left
-    :param right_images: ColumnDataSource or GlyphRenderer used to
-                         define RGBA images when toggle is set to right
-    """
-
-    def __init__(self, left_images, right_images):
-        self.left_images = left_images
-        self._left_cache = CachedRGBA(left_images)
-        self.right_images = right_images
-        self._right_cache = CachedRGBA(left_images)
-
-    def show_left(self):
-        """Show left image and hide right image"""
-        self.show(self.left_images)
-        self.hide(self.right_images)
-
-    def show_right(self):
-        """Show right image and hide left image"""
-        self.show(self.right_images)
-        self.hide(self.left_images)
-
-    def show(self, source):
-        """Show an image
-
-        .. note:: Caches existing alpha values if not already cached
-                  and exits since there is no alpha information to
-                  change
-        .. note:: Updates image alpha values in-place
-        """
-        if "_alpha" not in source.data:
-            source.data["_alpha"] = get_alpha(source)
-            return
-        # Restore alpha from cache
-        images = source.data["image"]
-        alphas = source.data["_alpha"]
-        for image, alpha in zip(images, alphas):
-            image[..., -1] = alpha
-        source.data["image"] = images
-
-    def hide(self, source):
-        """Hide an image
-
-        Set alpha values in RGBA arrays to zero, while keeping a
-        copy of the original values for restoration purposes
-
-        .. note:: Caches existing alpha values if not already cached
-        .. note:: Updates image alpha values in-place
-        """
-        if "_alpha" not in source.data:
-            source.data["_alpha"] = get_alpha(source)
-        # Set alpha to zero
-        images = source.data["image"]
-        for image in images:
-            image[..., -1] = 0
-        source.data["image"] = images
 
 
 class Slider(object):
