8639a6017cb6719ae8fa3df6e84a4f0a34f84389
tam203 <theo.mccaie@informaticslab.co.uk>
2018-08-15 11:49:46 +0100

    Fix auto deploy (#91)
    * delete pod so that new pod is created with latest code from GitHub
    
    * add conda bokeh install in place it will actually run (fingers crossed)

--- forest_lib/run_forest_server.sh
+++ forest_lib/run_forest_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+conda install bokeh=0.13.0 -y
 export S3_ROOT=$1
-bokeh serve --allow-websocket-origin ${FOREST_URL} --port=$2 $3/*
+bokeh serve --allow-websocket-origin ${FOREST_URL} --port=$2 $3/plot_sea_two_model_comparison
