1b230268c499adbb09ca121e5dcf80dd7ac1e841
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-08-21 16:02:42 +0100

    added a full screen functional JS solution

--- bokeh_apps/plot_sea_two_model_comparison/static/js/forest.js
+++ bokeh_apps/plot_sea_two_model_comparison/static/js/forest.js
@@ -4,15 +4,23 @@ let Forest = (function() {
     let bokehLoaded = function() {
         return Bokeh.documents.length > 0;
     };
+    let fullScreen = function(figure) {
+        figure.width = window.innerWidth;
+        figure.height = window.innerHeight;
+    };
     let main = function() {
         if (bokehLoaded()) {
             clearInterval(timerID);
         } else {
             return;
         }
-
-        // Bokeh has finished loading
-        console.log("forest.js: Bokeh loaded");
+        let roots = Bokeh.documents[0].roots();
+        let figures = roots.filter(root => root.name === "figure");
+        figures.map(fullScreen);
+        window.dispatchEvent(new Event('resize'));
+        window.addEventListener('resize', function() {
+            figures.map(fullScreen);
+        });
     };
     timerID = setInterval(main, 50);
     return {
