2578a014a551cf186c20aa08f26b8c8588aa6136
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-25 10:58:32 +0100

    refactoring towards reactive components

--- bokeh_apps/app/main.py
+++ bokeh_apps/app/main.py
@@ -11,6 +11,21 @@ script_dir = os.path.dirname(__file__)
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(script_dir))
 
 
+PARAMETERS = [
+    "Precipitation",
+    "Air temperature",
+    "Wind vectors",
+    "Wind and MSLP",
+    "Wind streamlines",
+    "MSLP",
+    "Cloud fraction",
+    "Accum. precip. 3hr",
+    "Accum. precip. 6hr",
+    "Accum. precip. 12hr",
+    "Accum. precip. 24hr",
+]
+
+
 def load_app(config_file):
     return App.load(config_file)
 
@@ -27,6 +42,10 @@ class App(object):
 
     def __call__(self, document):
         app(document, **self.kwargs)
+
+
+def on_change(attr, old, new):
+    print("change: {} {} {}".format(attr, old, new))
 
 
 def app(document, title=None, regions=None, models=None):
@@ -47,7 +66,12 @@ def app(document, title=None, regions=None, models=None):
         else:
             size = size + 5
         source.data["size"] = size
-    document.add_root(controls(models, regions))
+    models = [item["name"] for item in models]
+    regions = [item["name"] for item in regions]
+    region_drop_down = drop_down(regions, regions[0])
+    region_drop_down.on_change("value", on_change)
+    parameters = PARAMETERS
+    document.add_root(controls(models, region_drop_down, parameters))
     document.add_root(figure)
 
     filename = os.path.join(script_dir, "theme.yaml")
@@ -56,24 +80,29 @@ def app(document, title=None, regions=None, models=None):
     document.template = env.get_template("templates/index.html")
 
 
-def controls(models, regions):
+def controls(models, region_drop_down, parameters):
     checkbox = bokeh.models.CheckboxGroup(labels=["Link plots", "Activate slider"], active=[0])
     left_child = bokeh.layouts.column(
             plus_minus_row("Time"),
             plus_minus_row("Forecast"),
             plus_minus_row("Model run"),
-            drop_down(models, 0))
+            drop_down(models, models[0]))
     right_child = bokeh.layouts.column(
             plus_minus_row("Time"),
             plus_minus_row("Forecast"),
             plus_minus_row("Model run"),
-            drop_down(models, 1))
+            drop_down(models, models[1]))
     panels = [
      bokeh.models.Panel(child=left_child, title="Left"),
      bokeh.models.Panel(child=right_child, title="Right")
     ]
     tabs = bokeh.models.Tabs(tabs=panels)
-    return bokeh.layouts.column(checkbox, drop_down(regions, 0), tabs, name="btn")
+    return bokeh.layouts.column(
+            checkbox,
+            region_drop_down,
+            drop_down(parameters, parameters[0]),
+            tabs,
+            name="btn")
 
 
 def plus_minus_row(text):
@@ -95,12 +124,7 @@ def encode(name):
     return name.lower().replace(" ", "").replace(".", "")
 
 
-def drop_down(items, index=None):
-    names = [item["name"] for item in items]
-    if index is not None:
-        label = names[index]
-    else:
-        label = None
+def drop_down(names, label=None):
     return bokeh.models.Dropdown(menu=[
         (name, encode(name)) for name in names
     ], label=label)
