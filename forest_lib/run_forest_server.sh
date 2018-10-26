3be8bc62536432e7919cbc8cc55782dc10464f27
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-10-26 14:15:22 +0100

    changes needed to deploy custom Tornado server

--- forest_lib/run_forest_server.sh
+++ forest_lib/run_forest_server.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 conda install bokeh=0.13.0 -y
+
 export S3_ROOT=$1
-bokeh serve --allow-websocket-origin ${FOREST_URL} --port=$2 $3/plot_sea_two_model_comparison
+PORT=$2
+BOKEH_APP_DIR=$3
+
+# Tornado server for HIGHWAY and WCSSP
+${BOKEH_APP_DIR}/server.py \
+    --port=${PORT} \
+    --allow-websocket-origin ${FOREST_URL}
