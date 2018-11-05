ca706b46b3847f6d4f63cdeb301d7880b02eae8d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2018-11-05 12:02:02 +0000

    extended session lifetime to be 24h

--- forest_lib/run_forest_server.sh
+++ forest_lib/run_forest_server.sh
@@ -5,8 +5,10 @@ conda install bokeh=0.13.0 -y
 export S3_ROOT=$1
 PORT=$2
 BOKEH_APP_DIR=$3
+DAY_IN_MILLISECONDS=86400000
 
 # Tornado server for HIGHWAY and WCSSP
 ${BOKEH_APP_DIR}/server.py \
     --port=${PORT} \
-    --allow-websocket-origin ${FOREST_URL}
+    --allow-websocket-origin ${FOREST_URL} \
+    --unused-session-lifetime ${DAY_IN_MILLISECONDS}
