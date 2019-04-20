09d1b2dc8f8bfb7cf0124ea43cabe181e4f664eb
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-04-20 09:20:54 +0200

    added some invisible color settings

--- bokeh_apps/forest/colors.py
+++ bokeh_apps/forest/colors.py
@@ -2,6 +2,7 @@
 Helpers to choose color palette(s), limits etc.
 """
 import bokeh.palettes
+import bokeh.colors
 import bokeh.layouts
 from util import select
 
@@ -36,10 +37,26 @@ class Controls(object):
             active=[])
         self.checkbox.on_change("active", self.on_reverse)
 
+        # Invisible color settings
+        self.invisible_on = False
+        self.low = 0
+        self.invisible_checkbox = bokeh.models.CheckboxButtonGroup(
+            labels=["Invisible"],
+            active=[])
+        self.invisible_checkbox.on_change("active",
+                self.on_invisible_checkbox)
+        self.invisible_input = bokeh.models.TextInput(
+                title="Low:",
+                value="0")
+        self.invisible_input.on_change("value",
+                self.on_invisible_input)
+
         self.layout = bokeh.layouts.column(
                 self.names,
                 self.numbers,
-                self.checkbox)
+                self.checkbox,
+                self.invisible_checkbox,
+                self.invisible_input)
 
     def on_name(self, name):
         self.name = name
@@ -61,11 +78,21 @@ class Controls(object):
         self.render()
 
     def on_reverse(self, attr, old, new):
-        print(attr, old, new)
         if len(new) == 1:
             self.reverse = True
         else:
             self.reverse = False
+        self.render()
+
+    def on_invisible_checkbox(self, attr, old, new):
+        if len(new) == 1:
+            self.invisible_on = True
+        else:
+            self.invisible_on = False
+        self.render()
+
+    def on_invisible_input(self, attr, old, new):
+        self.low = float(new)
         self.render()
 
     def render(self):
@@ -77,4 +104,9 @@ class Controls(object):
         if self.reverse:
             palette = list(reversed(palette))
         print(palette)
+        if self.invisible_on:
+            low = self.low
+            color = bokeh.colors.RGB(0, 0, 0, a=0)
+            self.color_mapper.low_color = color
+            self.color_mapper.low = low
         self.color_mapper.palette = palette
