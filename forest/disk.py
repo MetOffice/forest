01e52f3015d0a9842764778b8856c141187f8d15
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-05-29 13:09:26 +0100

    attempt to scrape meta-data to locate fields

--- forest/disk.py
+++ forest/disk.py
@@ -9,12 +9,84 @@ class NoData(Exception):
     pass
 
 
-class GlobalUM(object):
+def scrape(path):
+    """All meta-data in single transaction"""
+    scheme = {
+        'dimensions': [],
+        'variables': {}
+    }
+    with netCDF4.Dataset(path) as dataset:
+        for d in dataset.dimensions:
+            scheme['dimensions'].append(d)
+        for v, obj in dataset.variables.items():
+            scheme['variables'][v] = {
+                'dimensions': obj.dimensions,
+                'attrs': {a: getattr(obj, a) for a in obj.ncattrs()}
+            }
+    return scheme
+
+
+def coordinate_variables(scheme):
+    """In-memory coordinate variable detection"""
+    coords = set()
+    for item in scheme.items():
+        attrs = item['attrs']
+        if 'coordinates' not in attrs:
+            continue
+        for c in attrs['coordinates'].split():
+            coords.add(c)
+    return coords
+
+
+def dimension_variables(scheme):
+    """In-memory dimension variable detection"""
+    names = set()
+    for item in scheme.items():
+        for d in scheme['dimensions']:
+            if d in scheme['variables']:
+                names.add(d)
+    return names
+
+
+def load_variables(path, names):
+    values = {}
+    with netCDF4.Dataset(path) as dataset:
+        for name in names:
+            try:
+                var = dataset.variables[name]
+            except KeyError:
+                continue
+            if 'time' in name:
+                datetimes = netCDF4.num2date(var[:], units=var.units)
+                values[name] = np.array(datetimes, dtype='datetime64[s]')
+            else:
+                values[name] = var[:]
+    return values
+
+
+class Locator(object):
     def __init__(self, paths):
         self.paths = np.asarray(paths)
         self.initial_times = np.array([
                 util.initial_time(p) for p in paths],
                 dtype='datetime64[s]')
+
+    def search(self, initial):
+        if isinstance(initial, str):
+            initial = np.datetime64(initial, 's')
+        return self.paths[self.initial_times == initial]
+
+
+class GlobalUM(object):
+    """Locator for collection of UM diagnostic files
+
+    Uses file naming convention and meta-data stored in
+    files to quickly look up file/index related to point
+    in space/time
+    """
+    def __init__(self, paths):
+        self.locator = Locator(paths)
+        self.paths = np.asarray(paths)
         self.valid_times = {}
         self.pressures = {}
         for path in paths:
@@ -29,28 +101,69 @@ class GlobalUM(object):
                 self.pressures[path] = dataset.variables[
                         'pressure'][:]
 
-    def path_index(self, initial, valid, pressure):
-        path, pts = self.path_points(initial, valid, pressure)
+    def search(self, *args, **kwargs):
+        return self.path_points(*args, **kwargs)
+
+    def path_index(self, variable, initial, valid, pressure):
+        path, pts = self.path_points(variable, initial, valid, pressure)
         return path, np.argmax(pts)
 
-    def path_points(self, initial, valid, pressure):
+    def path_points(self, variable, initial, valid, pressure):
         if isinstance(valid, str):
             valid = np.datetime64(valid, 's')
         paths = self.run_paths(initial)
         for path in paths:
-            pts = points(
-                    self.valid_times[path],
-                    self.pressures[path],
-                    valid,
-                    pressure)
-            if pts.any():
-                return path, pts
+            with netCDF4.Dataset(path) as dataset:
+                valid_times = self._valid_times(dataset, variable)
+                pressures = self._pressures(dataset, variable)
+                if pressures is not None:
+                    pts = points(
+                            valid_times,
+                            pressures,
+                            valid,
+                            pressure)
+                else:
+                    pts = time_points(
+                            valid_times,
+                            valid)
+                if pts.any():
+                    return path, pts
         raise NoData('initial: {} valid: {} pressure: {}'.format(initial, valid, pressure))
 
+    @staticmethod
+    def _pressures(dataset, variable):
+        """Search dataset for pressure axis"""
+        var = dataset.variables[variable]
+        for d in var.dimensions:
+            if d.startswith('pressure'):
+                if d in dataset.variables:
+                    return dataset.variables[d][:]
+        coords = var.coordinates.split()
+        for c in coords:
+            if c.startswith('pressure'):
+                return dataset.variables[c][:]
+
+    @staticmethod
+    def _valid_times(dataset, variable):
+        """Search dataset for time axis"""
+        var = dataset.variables[variable]
+        for d in var.dimensions:
+            if d.startswith('time'):
+                if d in dataset.variables:
+                    tvar = dataset.variables[d]
+                    return np.array(
+                        netCDF4.num2date(tvar[:], units=tvar.units),
+                        dtype='datetime64[s]')
+        coords = var.coordinates.split()
+        for c in coords:
+            if c.startswith('time'):
+                tvar = dataset.variables[c]
+                return np.array(
+                    netCDF4.num2date(tvar[:], units=tvar.units),
+                    dtype='datetime64[s]')
+
     def run_paths(self, initial):
-        if isinstance(initial, str):
-            initial = np.datetime64(initial, 's')
-        return self.paths[self.initial_times == initial]
+        return self.locator.search(initial)
 
 
 
@@ -72,15 +185,23 @@ def lengths(times, initial):
 
 def points(times, pressures, time, pressure):
     """Locate slice of array for time/pressure"""
+    return (
+            pressure_points(pressures, pressure) &
+            time_points(times, time))
+
+
+def pressure_points(pressures, pressure):
     ptol = 1
+    return np.abs(pressures - pressure) < ptol
+
+
+def time_points(times, time):
     ttol = np.timedelta64(15 * 60, 's')
     if times.dtype == 'O':
         times = times.astype('datetime64[s]')
     if isinstance(time, dt.datetime):
         time = np.datetime64(time, 's')
-    return (
-            (np.abs(pressures - pressure) < ptol) &
-            (np.abs(times - time) < ttol))
+    return np.abs(times - time) < ttol
 
 
 def pressure_index(pressures, pressure):
--- forest/test_disk.py
+++ forest/test_disk.py
@@ -2,7 +2,24 @@ import unittest
 import datetime as dt
 import numpy as np
 import netCDF4
+import os
 import disk
+
+
+def full_path(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def stash_variables(dataset):
+    """Find all variables with Stash codes"""
+    return [name for name, obj in dataset.variables.items()
+            if hasattr(obj, 'um_stash_source')]
+
+
+def pressures(dataset, name):
+    variable = dataset.variables[name]
+    dimensions = variable.dimensions
+    return dimensions
 
 
 class TestPattern(unittest.TestCase):
@@ -15,13 +32,156 @@ class TestPattern(unittest.TestCase):
         self.assertEqual(expect, result)
 
 
+class TestSearch(unittest.TestCase):
+    def test_search(self):
+        self.assertTrue(False)
+
+
+class TestLocator(unittest.TestCase):
+    def test_paths_given_date_outside_range_returns_empty_list(self):
+        locator = disk.Locator([
+            "20190101T0000Z.nc",
+            "20190101T0600Z.nc",
+            "20190101T1200Z.nc",
+            "20190101T1800Z.nc",
+            "20190102T0000Z.nc"
+        ])
+        after = np.datetime64('2019-01-02 06:00', 's')
+        result = locator.search(after)
+        self.assertEqual(result.tolist(), [])
+
+    def test_paths_given_date_in_range_returns_list(self):
+        locator = disk.Locator([
+            "a/prefix_20190101T0000Z.nc",
+            "b/prefix_20190101T0600Z.nc",
+            "c/prefix_20190101T1200Z.nc",
+            "d/prefix_20190101T1800Z.nc",
+            "e/prefix_20190102T0000Z.nc"
+        ])
+        time = np.datetime64('2019-01-01 18:00', 's')
+        result = locator.search(time)
+        expect = ["d/prefix_20190101T1800Z.nc"]
+        np.testing.assert_array_equal(expect, result)
+
+    def test_paths_given_date_matching_multiple_files(self):
+        locator = disk.Locator([
+            "a/prefix_20190101T0000Z.nc",
+            "b/prefix_20190101T0600Z.nc",
+            "c/prefix_20190101T1200Z_000.nc",
+            "c/prefix_20190101T1200Z_003.nc",
+            "c/prefix_20190101T1200Z_006.nc",
+            "c/prefix_20190101T1200Z_009.nc",
+            "d/prefix_20190101T1800Z.nc",
+            "e/prefix_20190102T0000Z.nc"
+        ])
+        result = locator.search('2019-01-01 12:00')
+        expect = [
+            "c/prefix_20190101T1200Z_000.nc",
+            "c/prefix_20190101T1200Z_003.nc",
+            "c/prefix_20190101T1200Z_006.nc",
+            "c/prefix_20190101T1200Z_009.nc",
+        ]
+        np.testing.assert_array_equal(expect, result)
+
+
+class TestGlobalUM(unittest.TestCase):
+    def setUp(self):
+        self.path = full_path("data/test_global_africa_20190513T0000Z_039.nc")
+        self.locator = disk.GlobalUM([self.path])
+        self.variables = [
+            'air_temperature',
+            'dew_point_temperature',
+            'geopotential_height',
+            'moisture_content_of_soil_layer',
+            'precipitation_flux',
+            'relative_humidity',
+            'relative_humidity_0',
+            'relative_humidity_1',
+            'soil_moisture_content',
+            'surface_temperature',
+            'toa_outgoing_longwave_flux',
+            'wet_bulb_potential_temperature',
+            'x_wind',
+            'x_wind_0',
+            'y_wind',
+            'y_wind_0'
+        ]
+
+    def test_stash_variables(self):
+        with netCDF4.Dataset(self.path) as dataset:
+            result = stash_variables(dataset)
+        expect = self.variables
+        self.assertEqual(result, expect)
+
+    def test_geopotential_height(self):
+        '''
+        float geopotential_height(dim0, latitude, longitude) ;
+                geopotential_height:standard_name = "geopotential_height" ;
+                geopotential_height:units = "m" ;
+                geopotential_height:um_stash_source = "m01s16i202" ;
+                geopotential_height:grid_mapping = "latitude_longitude" ;
+                geopotential_height:coordinates = "forecast_period_1 forecast_reference_time pressure_0 time_1" ;
+        '''
+        with netCDF4.Dataset(self.path) as dataset:
+            var = dataset.variables["time_1"]
+            times = netCDF4.num2date(var[:], units=var.units)
+            times = np.array(times, dtype='datetime64[s]')
+            pressures = dataset.variables['pressure_0'][:]
+
+        variable = "geopotential_height"
+        initial = dt.datetime(2019, 5, 13, 0)
+        valid = dt.datetime(2019, 5, 14, 15)
+        pressure = 1000
+        print(np.where(disk.points(times, pressures, valid, pressure)))
+        path, pts = self.locator.search(
+            variable,
+            initial,
+            valid,
+            pressure)
+        self.assertEqual(os.path.basename(path), os.path.basename(self.path))
+        np.testing.assert_array_equal(np.where(pts)[0], [48])
+
+    def test_relative_humidity(self):
+        variable = "relative_humidity"
+        initial = dt.datetime(2019, 5, 13, 0)
+        valid = dt.datetime(2019, 5, 14, 15)
+        pressure = 1000
+        path, pts = self.locator.path_points(variable, initial, valid, pressure)
+        self.assertEqual(os.path.basename(path), os.path.basename(self.path))
+        self.assertEqual(np.where(pts), ([26],))
+
+    def test_relative_humidity_0(self):
+        variable = "relative_humidity_0"
+        initial = dt.datetime(2019, 5, 13, 0)
+        valid = dt.datetime(2019, 5, 14, 15)
+        pressure = 1000
+        path, pts = self.locator.path_points(variable, initial, valid, pressure)
+        self.assertEqual(os.path.basename(path), os.path.basename(self.path))
+        self.assertEqual(np.where(pts), ([23],))
+
+    def test_relative_humidity_1(self):
+        variable = "relative_humidity_1"
+        initial = dt.datetime(2019, 5, 13, 0)
+        valid = dt.datetime(2019, 5, 14, 15)
+        pressure = 1000
+        path, pts = self.locator.path_points(variable, initial, valid, pressure)
+        self.assertEqual(os.path.basename(path), os.path.basename(self.path))
+        self.assertEqual(np.where(pts), ([26],))
+
+    def test_soil_moisture_content(self):
+        with netCDF4.Dataset(self.path) as dataset:
+            result = self.locator._valid_times(dataset, 'soil_moisture_content')
+        expect = np.array('2019-05-14 15:00', dtype='datetime64[s]')
+        self.assertEqual(result, expect)
+
+
 @unittest.skip("green light")
 class TestIndex(unittest.TestCase):
     def setUp(self):
         self.initial = dt.datetime(2019, 4, 30, 6)
-        self.path = "/Users/andrewryan/cache/global_africa_20190430T0600Z_umglaa_pa009.nc"
+        self.path = full_path("data/test_global_africa_20190513T0000Z_039.nc")
         with netCDF4.Dataset(self.path) as dataset:
-            var = dataset.variables["time"]
+            var = dataset.variables["time_1"]
             self.times = netCDF4.num2date(var[:], units=var.units)
             self.pressures = dataset.variables["pressure"][:]
 
