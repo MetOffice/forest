3e8f1cbde79ef1dca56a3bd8cca26d89a6015f05
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-05-29 14:06:38 +0100

    simplified deployment to run server/run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
-conda install bokeh=1.0.4 -y
-
 export S3_ROOT=$1
 PORT=$2
-BOKEH_APP_DIR=$3
+REPO_DIR=$3
 DAY_IN_MILLISECONDS=86400000
 PING_MILLISECONDS=10000
+
+conda install -y --file ${REPO_DIR}/conda-spec.txt
 
 USE_CUSTOM=False
 if [[ "$USE_CUSTOM" == "True" ]] ; then
@@ -19,8 +18,8 @@ if [[ "$USE_CUSTOM" == "True" ]] ; then
 else
     # Bokeh server for HIGHWAY only
     FOREST_DIR=${S3_ROOT}/stephen-sea-public-london \
-    FOREST_CONFIG_FILE=${BOKEH_APP_DIR}/../forest/highway.yaml \
-    bokeh serve ${BOKEH_APP_DIR}/../forest \
+    FOREST_CONFIG_FILE=${REPO_DIR}/forest/highway.yaml \
+    bokeh serve ${REPO_DIR}/forest \
         --port ${PORT} \
         --allow-websocket-origin ${FOREST_URL} \
         --unused-session-lifetime ${DAY_IN_MILLISECONDS} \
