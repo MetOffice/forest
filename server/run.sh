b1c02b431e5ca44b1609725a5d230b4acb3b791c
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-05-30 18:34:47 +0100

    prepare to deploy using database solution

--- server/run.sh
+++ server/run.sh
@@ -7,6 +7,7 @@ PING_MILLISECONDS=10000
 
 conda install -y --file ${REPO_DIR}/conda-spec.txt
 
+
 USE_CUSTOM=False
 if [[ "$USE_CUSTOM" == "True" ]] ; then
     # Tornado server for HIGHWAY and WCSSP
@@ -17,11 +18,15 @@ if [[ "$USE_CUSTOM" == "True" ]] ; then
         --keep-alive ${PING_MILLISECONDS}
 else
     # Bokeh server for HIGHWAY only
+    # FOREST_CONFIG_FILE=${REPO_DIR}/forest/highway.yaml \
     FOREST_DIR=${S3_ROOT}/stephen-sea-public-london \
-    FOREST_CONFIG_FILE=${REPO_DIR}/forest/highway.yaml \
     bokeh serve ${REPO_DIR}/forest \
         --port ${PORT} \
         --allow-websocket-origin ${FOREST_URL} \
         --unused-session-lifetime ${DAY_IN_MILLISECONDS} \
-        --keep-alive ${PING_MILLISECONDS}
+        --keep-alive ${PING_MILLISECONDS} \
+        --args \
+        --directory ${FOREST_DIR}/model_data \
+        --database ${FOREST_DIR}/forest-informaticslab.db \
+        --config-file ${REPO_DIR}/forest/config.yaml
 fi
