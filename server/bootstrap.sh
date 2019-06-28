93312c597378499a34e38a622077f34a5afb1388
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-06-28 14:20:41 +0100

    mount bucket and point forest at database

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -2,7 +2,6 @@
 yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git docker
 service docker start
 usermod -a -G docker ec2-user
-mkdir /s3
 exec sudo -u ec2-user /bin/bash - <<EOF
     cd
     git clone https://github.com/s3fs-fuse/s3fs-fuse
@@ -17,11 +16,14 @@ exec sudo -u ec2-user /bin/bash - <<EOF
     chmod +x goofys
     sudo mv goofys /usr/bin/goofys
 
+    mkdir -p ~/buckets/met-office-rmed-forest
+    goofys met-office-rmed-forest ~/buckets/met-office-rmed-forest
+
     cd
     git clone https://github.com/informatics-lab/forest.git
     docker run \
       -p 80:8080 \
       -v /home/ec2-user/forest:/repo/forest \
-      -v /s3:/s3 \
-      informaticslab/forest bash -c '. /repo/forest/server/run-ec2.sh /repo/forest'
+      -v /home/ec2-user/buckets:/buckets \
+      informaticslab/forest bash -c '. /repo/forest/server/run-ec2.sh /repo/forest /buckets'
 EOF
--- server/run-ec2.sh
+++ server/run-ec2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # EC2 instance bokeh serve command
 REPO_DIR=$1
-touch ${REPO_DIR}/forest/empty.db
+BUCKET_DIR=$2
 bokeh serve \
     --port 8080 \
     --allow-websocket-origin forest-future.informaticslab.co.uk \
     ${REPO_DIR}/forest \
     --args \
-        --database ${REPO_DIR}/forest/empty.db \
+        --database ${BUCKET_DIR}/met-office-rmed-forest/forest-informaticslab.db \
         --config-file ${REPO_DIR}/forest/config.yaml
