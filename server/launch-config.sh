4baf5fc679458cbc3ef66dadf3a5b05716d38087
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-06-28 11:14:29 +0100

    add FUSE build instructions

--- server/launch-config.sh
+++ server/launch-config.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-yum install git docker -y
+yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git docker
 service docker start
 usermod -a -G docker ec2-user
 mkdir /s3
 exec sudo -u ec2-user /bin/bash - <<EOF
+    cd
+    git clone https://github.com/s3fs-fuse/s3fs-fuse
+    cd s3fs-fuse
+    ./autogen.sh
+    ./configure --prefix=/usr --with-openssl
+    make
+    sudo make install
+
     cd
     git clone https://github.com/informatics-lab/forest.git
     docker run \
