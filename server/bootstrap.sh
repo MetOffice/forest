764329fa577c4e86d7c4362af6162291d1277565
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-06-28 11:40:03 +0100

    extracted launch config details into bootstrap.sh

---
+++ server/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git docker
+service docker start
+usermod -a -G docker ec2-user
+mkdir /s3
+exec sudo -u ec2-user /bin/bash - <<EOF
+    cd
+    git clone https://github.com/s3fs-fuse/s3fs-fuse
+    cd s3fs-fuse
+    ./autogen.sh
+    ./configure --prefix=/usr --with-openssl
+    make
+    sudo make install
+
+    cd
+    git clone https://github.com/informatics-lab/forest.git
+    docker run \
+      -p 80:8080 \
+      -v /home/ec2-user/forest:/repo/forest \
+      -v /s3:/s3 \
+      informaticslab/forest bash -c '. /repo/forest/server/run-ec2.sh /repo/forest'
+EOF
