36a14364588c2ae57cbc11eba6b8ae2074847f2a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-07-29 15:18:06 +0100

    add world app to display map

--- server/run-ec2.sh
+++ server/run-ec2.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 REPO_DIR=$1
+BUCKET_DIR=$2
 bokeh serve \
     --port 8080 \
     --allow-websocket-origin forest.informaticslab.co.uk \
-     ${REPO_DIR}/apps/hello
-
-
-# # EC2 instance bokeh serve command
-# REPO_DIR=$1
-# BUCKET_DIR=$2
-# bokeh serve \
-#     --port 8080 \
-#     --allow-websocket-origin forest-future.informaticslab.co.uk \
-#     ${REPO_DIR}/forest \
-#     --args \
-#         --database ${BUCKET_DIR}/met-office-rmed-forest/forest-informaticslab.db \
-#         --config-file ${REPO_DIR}/forest/config.yaml
+     ${REPO_DIR}/apps/world
