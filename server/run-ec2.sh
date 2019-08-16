12ae6341d51c062a160e42d473161871051851d3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-08-16 12:05:29 +0100

    point forest at latest infrastructure

--- server/run-ec2.sh
+++ server/run-ec2.sh
@@ -4,4 +4,7 @@ BUCKET_DIR=$2
 bokeh serve \
     --port 8080 \
     --allow-websocket-origin forest.informaticslab.co.uk \
-     ${REPO_DIR}/apps/forest
+     ${REPO_DIR}/forest \
+         --args \
+         --config ${REPO_DIR}/forest/config.yaml \
+         --database ${BUCKET_DIR}/met-office-rmed-forest/forest-informaticslab.db
