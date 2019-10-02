b0745e84bd655a98e08ae39a5f0275e4fedb84e4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-10-02 16:25:40 +0100

    Add package_data to distribute tutorial (#151)
    * Add package_data to distribute tutorial
    
    * Roll version forward

--- forest/__init__.py
+++ forest/__init__.py
@@ -3,7 +3,7 @@ This version of FOREST consists of a main.py program served
 by Bokeh. Later releases will focus on dividing the API into re-usable
 components.
 """
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 from .config import *
 from . import (
