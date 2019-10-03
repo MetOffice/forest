55c9f0256c7f28c14c0b63c30710cb9df6304d28
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-10-03 11:15:11 +0100

    added key press js/css/html machinery

--- forest/keys.py
+++ forest/keys.py
@@ -1,4 +1,5 @@
 """Key press interactivity"""
+import bokeh.models
 from forest.observe import Observable
 from forest.export import export
 
@@ -21,7 +22,33 @@ def press(code):
 
 @export
 class KeyPress(Observable):
-    """Key press observable"""
+    """Key press server-side observable
+
+    .. note:: KeyPress.hidden_button must be added to the
+              document to allow JS hack to initialise callbacks
+    """
+    def __init__(self):
+        source = bokeh.models.ColumnDataSource({
+            'keys': []
+        })
+        source.on_change('data', self.on_change)
+        custom_js = bokeh.models.CustomJS(args=dict(source=source), code="""
+            if (typeof keyPressOn === 'undefined') {
+                document.onkeydown = function(e) {
+                    source.data = {
+                        'keys': [e.code]
+                    }
+                    source.change.emit()
+                }
+                // Global to prevent multiple onkeydown callbacks
+                keyPressOn = true
+            }
+        """)
+        self.hidden_button = bokeh.models.Button(
+                css_classes=['keypress-hidden-btn'])
+        self.hidden_button.js_on_click(custom_js)
+        super().__init__()
+
     def on_change(self, attr, old, new):
         code = new[0]
         self.notify(press(code))
