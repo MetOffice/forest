b3bdefb861aeeccef94e86e291eedb299f5d7e43
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-10-04 12:08:42 +0100

    update docstrings to document redux and keypress behaviour

--- forest/redux.py
+++ forest/redux.py
@@ -1,4 +1,31 @@
-"""Redux design pattern"""
+"""
+Redux design pattern
+--------------------
+
+As applications grow the number of components
+vying to keep their state synchronised becomes unwieldy.
+A better way to manage state is centralise it
+so that there is a single source of truth. The
+consequence of which is easy to replay, serialise
+and rehydrate applications.
+
+For further reading, check out the redux.js docs
+
+* https://redux.js.org/introduction/motivation
+
+It also enables an easily unit testable system
+since state updates are pure function with easy to
+separate single responsibility components. Middleware
+components are plug and play, drop one in to add
+behaviour to your app without fear of altering
+existing behaviour.
+
+.. autoclass:: Store
+   :members:
+
+.. autofunction:: middleware
+
+"""
 import copy
 from functools import wraps
 from forest.observe import Observable
@@ -10,7 +37,17 @@ __all__ = []
 
 @export
 def middleware(f):
-    """Curries functions to satisfy middleware signature"""
+    """Curries functions to satisfy middleware signature
+
+    This decorator supports both instance methods and functions
+    signatures. It nests functions into a format used by
+    :class:`Store` to patch its dispatch method.
+
+    * Method signature `method(self, store, next_dispatch, action)`
+    * Function signature `func(store, next_dispatch, action)`
+
+    :param: function to decorate should have middleware signature
+    """
     @wraps(f)
     def outer(*args):
         def inner(next_dispatch):
@@ -23,6 +60,27 @@ def middleware(f):
 
 @export
 class Store(Observable):
+    """Observable state container
+
+    The redux design pattern is a simple way to
+    keep track of state changes. A reducer combines an action with
+    the current state to produce the next state. The reducer
+    should be a pure function in the sense of not having side effects.
+
+    Non-pure behaviour can be incorporated through the use of
+    middleware. Middleware takes an action and either passes it on,
+    filters it, enriches it or emits new actions.
+
+    The store is an observable that emits states, views can register
+    themselves with the store to receive the latest states as and
+    when they are created.
+
+    >>> store.subscribe(listener)
+
+    :param reducer: function combines action and state to produce new state
+    :param initial_state: optional initial state, default {}
+    :param middlewares: list of middleware functions that intercept actions
+    """
     def __init__(self, reducer, initial_state=None, middlewares=None):
         self.reducer = reducer
         self.state = initial_state if initial_state is not None else {}
@@ -35,5 +93,9 @@ class Store(Observable):
         super().__init__()
 
     def dispatch(self, action):
+        """Apply reducer and notify listeners of new state
+
+        :param action: plain dict consumed by the reducer
+        """
         self.state = self.reducer(self.state, action)
         self.notify(self.state)
