8e21e6af9aaac9a160142b111377e32143a356a2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-10-04 16:11:34 +0100

    add tests to cover replace_dir behaviour

--- test/test_main.py
+++ test/test_main.py
@@ -16,3 +16,33 @@ def test_build_loader():
     group = forest.config.FileGroup("name", "pattern")
     args = main.parse_args.parse_args(["file_20190101T0000Z.nc"])
     main.build_loader(group, args)
+
+
+def test_replace_dir_given_args_dir_only():
+    actual = main.replace_dir("args/dir", None)
+    expected = "args/dir"
+    assert actual == expected
+
+
+def test_replace_dir_given_group_dir_only():
+    actual = main.replace_dir(None, "group/dir")
+    expected = "group/dir"
+    assert actual == expected
+
+
+def test_replace_dir_given_relative_group_dir_appends_to_args_dir():
+    actual = main.replace_dir("args/dir", "leaf")
+    expected = "args/dir/leaf"
+    assert actual == expected
+
+
+def test_replace_dir_given_absolute_group_dir_overrides_rel_args_dir():
+    actual = main.replace_dir("args/relative", "/group/absolute")
+    expected = "/group/absolute"
+    assert actual == expected
+
+
+def test_replace_dir_given_absolute_group_dir_overrides_abs_args_dir():
+    actual = main.replace_dir("/args/absolute", "/group/absolute")
+    expected = "/group/absolute"
+    assert actual == expected
