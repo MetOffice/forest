7d1a581a1e8e492fe8d5713071819ce74c87d89d
anke.finnenkoetter <afinnen@elsra2.metoffice.gov.uk>
2019-10-25 11:56:49 +0100

    Fixed all of the widgets updating with any change, no longer creating random space

--- forest/colors.py
+++ forest/colors.py
@@ -14,6 +14,12 @@ class Controls(object):
         self.number = number
         self.palettes = bokeh.palettes.all_palettes
         self.color_mapper = color_mapper
+        try:  # FCL: Refresh palette from Controls call
+            self.color_mapper.palette = bokeh.palettes.all_palettes[name][number]
+        except KeyError:  # If it doesn't have a palette with this number of steps, choose the max number of steps
+            # (though alternatively we could also pick the nearest value to the one specified)
+            self.color_mapper.palette = bokeh.palettes.all_palettes[name][max(bokeh.palettes.all_palettes[name].keys())]
+
         self.cbar_min = color_mapper.low
         self.cbar_max = color_mapper.high
 
@@ -35,10 +41,12 @@ class Controls(object):
         self.numbers.on_change("value", self.on_number)
 
         # Option to set customised discrete colourscale intervals
-        # (None of this is actually working... )
+        # (None of this is actually linked up to the colour bar configuration... )
         # length of list depending on setting in numbers
-        # Want list input only appearing when cbar_discrete is selected
-        # Want cbar_min / max to disappear (those values are specified as first and last values of interval list
+        # Want list input only appearing when cbar_discrete is selected --> Done
+        # Want cbar_min / max to disappear (those values are specified as first and last values of interval list --> Done
+        # Want list input to appear below the discrete checkbox (and could do with a better display option...)
+
         self.cbar_discrete = False
         # Intervals default to linear spacing of min/max with the number of
         # intervals epending on number set in colour steps setting
@@ -98,14 +106,12 @@ class Controls(object):
                 self.numbers,
                 self.cbar_discrete_checkbox,
                 self.checkbox,
-                self.cbar_min_input,
-                self.cbar_max_input,
                 self.invisible_checkbox]
         self.invisible_show = [self.invisible_input_lower,
                                self.invisible_input_upper]
         self.discrete_show = [self.cbar_discrete_input]
         self.min_max_show = [self.cbar_min_input, self.cbar_max_input]
-        self.layout = bokeh.layouts.column(self.default_widges)
+        self.layout = bokeh.layouts.column(self.default_widges+self.min_max_show)
         # self.layout = bokeh.layouts.column(
         #         self.names,
         #         self.numbers,
@@ -153,8 +159,13 @@ class Controls(object):
     def on_cbar_discrete(self, attr, old, new):
         if len(new) == 1:
             self.cbar_discrete = True
+            # Insert discrete value input and remove cbar min/max input
+            self.layout.children = [lc for lc in self.layout.children + self.discrete_show
+                                    if lc not in self.min_max_show]
         else:
             self.cbar_discrete = False
+            self.layout.children = [lc for lc in self.layout.children + self.min_max_show
+                                    if lc not in self.discrete_show]
         self.render()
 
     def on_cbar_discrete_input(self, attr, old, new):
@@ -162,10 +173,14 @@ class Controls(object):
         self.render()
 
     def on_invisible_checkbox(self, attr, old, new):
+        # Input fields currently not inserted in the most sensible order
         if len(new) == 1:
             self.invisible_on = True
+            self.layout.children = self.layout.children + self.invisible_show
         else:
             self.invisible_on = False
+            self.layout.children = [lc for lc in self.layout.children
+                                    if lc not in self.invisible_show]
         self.render()
 
     def on_invisible_input_lower(self, attr, old, new):
@@ -178,7 +193,6 @@ class Controls(object):
 
     def render(self):
         if self.name is None:
-
             return
         if self.number is None:
             return
@@ -190,16 +204,4 @@ class Controls(object):
             color = bokeh.colors.RGB(0, 0, 0, a=0)
             self.color_mapper.low_color = color
             self.color_mapper.low = lower
-            self.layout.children = self.layout.children + self.invisible_show
-        else:
-            self.layout.children = [lc for lc in self.layout.children
-                                    if lc not in self.invisible_show]
-        if self.cbar_discrete:
-            # Insert discrete value input and remove cbar min/max input
-            self.layout.children = [lc for lc in self.layout.children + self.discrete_show
-                                    if lc not in self.min_max_show]
-        else:
-            self.layout.children = [lc for lc in self.layout.children + self.min_max_show
-                                    if lc not in self.discrete_show]
-
         self.color_mapper.palette = palette
