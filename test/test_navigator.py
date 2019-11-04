2ca2c74d7899344e91c94c5d6ebf07d7ffb25017
Richard Hattersley <rhattersley@gmail.com>
2019-11-04 15:58:51 +0000

    Extend tests for navigate module (#194)
    * Add remaining Navigator tests.
    
    * Add FileSystemNavigator tests

--- test/test_navigator.py
+++ test/test_navigator.py
@@ -1,9 +1,10 @@
 from unittest.mock import Mock, call, patch, sentinel
-import datetime as dt
+
 import numpy as np
 import pytest
 
-import forest
+import forest.exceptions
+
 import forest.navigate as navigate
 
 
@@ -72,50 +73,218 @@ def test_Navigator_expand_paths__with_dir(join, expanduser, glob):
     assert paths == sentinel.paths
 
 
-@pytest.mark.skip("use real unified model file")
-def test_unified_model_navigator():
-    paths = ["unified.nc"]
-    navigator = forest.navigate.FileSystem(
-            paths,
-            coordinates=forest.unified_model.Coordinates())
-    result = navigator.initial_times("*.nc")
-    expect = []
-    assert expect == result
+def test_Navigator_variables():
+    sub_navigator = Mock()
+    sub_navigator.variables.return_value = sentinel.variables
+    navigator = Mock(_navigators={sentinel.pattern: sub_navigator})
+
+    result = navigate.Navigator.variables(navigator, sentinel.pattern)
+
+    sub_navigator.variables.assert_called_once_with(sentinel.pattern)
+    assert result == sentinel.variables
 
 
-@pytest.fixture
-def rdt_navigator():
-    paths = ["/some/file_201901010000.json"]
-    return forest.navigate.FileSystemNavigator.from_file_type(paths, "rdt")
+def test_Navigator_initial_times():
+    sub_navigator = Mock()
+    sub_navigator.initial_times.return_value = sentinel.initial_times
+    navigator = Mock(_navigators={sentinel.pattern: sub_navigator})
+
+    result = navigate.Navigator.initial_times(navigator, sentinel.pattern,
+                                              sentinel.variable)
+
+    sub_navigator.initial_times.assert_called_once_with(
+        sentinel.pattern, variable=sentinel.variable)
+    assert result == sentinel.initial_times
 
 
-def test_rdt_navigator_valid_times_given_single_file(rdt_navigator):
-    paths = ["/some/rdt_201901010000.json"]
-    navigator = forest.navigate.FileSystemNavigator.from_file_type(paths, "rdt")
-    actual = navigator.valid_times("*.json", None, None)
-    expected = ["2019-01-01 00:00:00"]
-    assert actual == expected
+def test_Navigator_valid_times():
+    sub_navigator = Mock()
+    sub_navigator.valid_times.return_value = sentinel.valid_times
+    navigator = Mock(_navigators={sentinel.pattern: sub_navigator})
+
+    result = navigate.Navigator.valid_times(navigator, sentinel.pattern,
+                                            sentinel.variable,
+                                            sentinel.initial_time)
+
+    sub_navigator.valid_times.assert_called_once_with(
+        sentinel.pattern, sentinel.variable, sentinel.initial_time)
+    assert result == sentinel.valid_times
 
 
-def test_rdt_navigator_valid_times_given_multiple_files(rdt_navigator):
-    paths = [
-            "/some/rdt_201901011200.json",
-            "/some/rdt_201901011215.json",
-            "/some/rdt_201901011230.json"
-    ]
-    navigator = forest.navigate.FileSystemNavigator.from_file_type(paths, "rdt")
-    actual = navigator.valid_times(paths[1], None, None)
-    expected = ["2019-01-01 12:15:00"]
-    np.testing.assert_array_equal(actual, expected)
+def test_Navigator_pressures():
+    sub_navigator = Mock()
+    sub_navigator.pressures.return_value = sentinel.pressures
+    navigator = Mock(_navigators={sentinel.pattern: sub_navigator})
+
+    result = navigate.Navigator.pressures(navigator, sentinel.pattern,
+                                          sentinel.variable,
+                                          sentinel.initial_time)
+
+    sub_navigator.pressures.assert_called_once_with(
+        sentinel.pattern, sentinel.variable, sentinel.initial_time)
+    assert result == sentinel.pressures
 
 
-def test_rdt_navigator_variables(rdt_navigator):
-    assert rdt_navigator.variables("*.json") == ["RDT"]
+def test_FileSystemNavigator_init():
+    navigator = navigate.FileSystemNavigator(sentinel.paths, sentinel.coords)
+    assert navigator.paths == sentinel.paths
+    assert navigator.coordinates == sentinel.coords
 
 
-def test_rdt_navigator_initial_times(rdt_navigator):
-    assert rdt_navigator.initial_times("*.json") == ["2019-01-01 00:00:00"]
+@patch('forest.unified_model.Coordinates')
+def test_FileSystemNavigator_init__no_coords(coordinates_cls):
+    coordinates_cls.return_value = sentinel.coordinates
+
+    navigator = navigate.FileSystemNavigator(sentinel.paths)
+
+    coordinates_cls.assert_called_once_with()
+    assert navigator.paths == sentinel.paths
+    assert navigator.coordinates == sentinel.coordinates
 
 
-def test_rdt_navigator_pressures(rdt_navigator):
-    assert rdt_navigator.pressures("*.json", None, None) == []
+@patch('forest.rdt.Coordinates')
+def test_FileSystemNavigator_from_file_type__rdt(coordinates_cls):
+    coordinates_cls.return_value = sentinel.coordinates
+
+    navigator = navigate.FileSystemNavigator.from_file_type(sentinel.paths,
+                                                            'rDt')
+
+    coordinates_cls.assert_called_once_with()
+    assert navigator.paths == sentinel.paths
+    assert navigator.coordinates == sentinel.coordinates
+
+
+@patch('forest.eida50.Coordinates')
+def test_FileSystemNavigator_from_file_type__eida50(coordinates_cls):
+    coordinates_cls.return_value = sentinel.coordinates
+
+    navigator = navigate.FileSystemNavigator.from_file_type(sentinel.paths,
+                                                            'EiDa50')
+
+    coordinates_cls.assert_called_once_with()
+    assert navigator.paths == sentinel.paths
+    assert navigator.coordinates == sentinel.coordinates
+
+
+@patch('forest.gridded_forecast.Navigator')
+def test_FileSystemNavigator_from_file_type__griddedforecast(navigator_cls):
+    navigator_cls.return_value = sentinel.navigator
+
+    navigator = navigate.FileSystemNavigator.from_file_type(sentinel.paths,
+                                                            'grIDdeDforeCAST')
+
+    navigator_cls.assert_called_once_with(sentinel.paths)
+    assert navigator == sentinel.navigator
+
+
+@patch('forest.ghrsstl4.Navigator')
+def test_FileSystemNavigator_from_file_type__ghrsstl4(navigator_cls):
+    navigator_cls.return_value = sentinel.navigator
+
+    navigator = navigate.FileSystemNavigator.from_file_type(sentinel.paths,
+                                                            'ghrssTL4')
+
+    navigator_cls.assert_called_once_with(sentinel.paths)
+    assert navigator == sentinel.navigator
+
+
+@patch('forest.unified_model.Coordinates')
+def test_FileSystemNavigator_from_file_type__unified_model(coordinates_cls):
+    coordinates_cls.return_value = sentinel.coordinates
+
+    navigator = navigate.FileSystemNavigator.from_file_type(sentinel.paths,
+                                                            'UNIFIED_model')
+
+    coordinates_cls.assert_called_once_with()
+    assert navigator.paths == sentinel.paths
+    assert navigator.coordinates == sentinel.coordinates
+
+
+def test_FileSystemNavigator_from_file_type__unrecognised():
+    with pytest.raises(Exception, match='Unrecognised file type'):
+        navigate.FileSystemNavigator.from_file_type(sentinel.paths, 'FOO')
+
+
+def test_FileSystemNavigator_variables():
+    navigator = Mock(paths=['first', 'second', 'third', 'air'])
+    navigator.coordinates.variables.side_effect = [['one', 'two'], ['three'],
+                                                   ['two', 'five']]
+
+    variables = navigate.FileSystemNavigator.variables(navigator, '*ir*')
+
+    assert navigator.coordinates.variables.mock_calls == [call('first'),
+                                                          call('third'),
+                                                          call('air')]
+    assert variables == ['five', 'one', 'three', 'two']
+
+
+def test_FileSystemNavigator_initial_times():
+    navigator = Mock(paths=['first', 'second', 'third', 'air', 'bird'])
+    navigator.coordinates.initial_time.side_effect = [
+        'last', None, forest.exceptions.InitialTimeNotFound, 'first']
+
+    variables = navigate.FileSystemNavigator.initial_times(navigator, '*ir*')
+
+    assert navigator.coordinates.initial_time.mock_calls == [call('first'),
+                                                             call('third'),
+                                                             call('air'),
+                                                             call('bird')]
+    assert variables == ['first', 'last']
+
+
+def test_FileSystemNavigator_valid_times():
+    navigator = Mock(paths=['first', 'second', 'third', 'air', 'bird'])
+    navigator.coordinates.valid_times.side_effect = [
+        [2, 3, 4], None, forest.exceptions.ValidTimesNotFound, [1, 2, 5, 6]]
+
+    valid_times = navigate.FileSystemNavigator.valid_times(
+        navigator, '*ir*', sentinel.variable, sentinel.initial_time)
+
+    assert navigator.coordinates.valid_times.mock_calls == [
+        call('first', sentinel.variable),
+        call('third', sentinel.variable),
+        call('air', sentinel.variable),
+        call('bird', sentinel.variable)]
+    assert np.all(valid_times == [1, 2, 3, 4, 5, 6])
+
+
+def test_FileSystemNavigator_valid_times__empty():
+    navigator = Mock(paths=['first'])
+    navigator.coordinates.valid_times.return_value = None
+
+    valid_times = navigate.FileSystemNavigator.valid_times(
+        navigator, '*ir*', sentinel.variable, sentinel.initial_time)
+
+    assert navigator.coordinates.valid_times.mock_calls == [
+        call('first', sentinel.variable)]
+    assert valid_times == []
+
+
+def test_FileSystemNavigator_pressures():
+    navigator = Mock(paths=['first', 'second', 'third', 'air', 'bird'])
+    navigator.coordinates.pressures.side_effect = [
+        [2, 3, 4], None, forest.exceptions.PressuresNotFound, [1, 2, 5, 6]]
+
+    pressures = navigate.FileSystemNavigator.pressures(navigator, '*ir*',
+                                                       sentinel.variable,
+                                                       sentinel.initial_time)
+
+    assert navigator.coordinates.pressures.mock_calls == [
+        call('first', sentinel.variable),
+        call('third', sentinel.variable),
+        call('air', sentinel.variable),
+        call('bird', sentinel.variable)]
+    assert np.all(pressures == [1, 2, 3, 4, 5, 6])
+
+
+def test_FileSystemNavigator_pressures__empty():
+    navigator = Mock(paths=['first'])
+    navigator.coordinates.pressures.return_value = None
+
+    pressures = navigate.FileSystemNavigator.pressures(navigator, '*ir*',
+                                                       sentinel.variable,
+                                                       sentinel.initial_time)
+
+    assert navigator.coordinates.pressures.mock_calls == [
+        call('first', sentinel.variable)]
+    assert pressures == []
