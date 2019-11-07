215a064dfca673751dbd4f14369f4a4f84b0d55a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-11-07 13:39:09 +0000

    refactor test_parse_args to use pytest

--- test/test_parse_args.py
+++ test/test_parse_args.py
@@ -1,31 +1,21 @@
-import unittest
+import pytest
 from forest.parse_args import parse_args
 
 
-class TestParseArgs(unittest.TestCase):
-    def test_directory_returns_none_by_default(self):
-        argv = [
-            "--database", "file.db",
-            "--config-file", "file.yml"]
-        self.check(argv, "directory", None)
+def test_no_files_or_config_raises_system_exit():
+    with pytest.raises(SystemExit):
+        parse_args([])
 
-    def test_directory_returns_value(self):
-        argv = [
-            "--directory", "/some",
-            "--database", "file.db",
-            "--config-file", "file.yml"]
-        self.check(argv, "directory", "/some")
 
-    def test_files(self):
-        self.check(["file.json"], "files", ["file.json"])
-
-    def test_file_type(self):
-        self.check(["--file-type", "rdt", "file.json"], "file_type", "rdt")
-
-    def test_no_files_or_config_raises_system_exit(self):
-        with self.assertRaises(SystemExit):
-            parse_args([])
-
-    def check(self, argv, attr, expect):
-        result = getattr(parse_args(argv), attr)
-        self.assertEqual(expect, result)
+@pytest.mark.parametrize("argv,attr,expect", [
+    (["file.json"], "files", ["file.json"]),
+    (["--file-type", "rdt", "file.json"], "file_type", "rdt"),
+    (["--directory", "/some",
+      "--database", "file.db",
+      "--config-file", "file.yml"], "directory", "/some"),
+    (["--database", "file.db",
+      "--config-file", "file.yml"], "directory", None)
+])
+def test_parse_args(argv, attr, expect):
+    result = getattr(parse_args(argv), attr)
+    assert expect == result
