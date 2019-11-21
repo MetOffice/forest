9cb8154dab38b876f26902f2d44df0af8ff51fb4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-11-21 09:59:39 +0000

    add a test to cover MapperLimits

--- test/test_colors.py
+++ test/test_colors.py
@@ -1,5 +1,6 @@
-from forest import colors
+from forest import colors, main
 import bokeh.models
+import numpy as np
 
 
 def test_color_controls():
@@ -8,3 +9,17 @@ def test_color_controls():
     number = 3
     controls = colors.Controls(color_mapper, name, number)
     controls.render()
+
+
+def test_mapper_limits():
+    attr, old, new = None, None, None  # not used by callback
+    color_mapper = bokeh.models.LinearColorMapper()
+    source = bokeh.models.ColumnDataSource({
+        "image": [
+            np.arange(9).reshape(3, 3)
+        ]
+    })
+    mapper_limits = main.MapperLimits([source], color_mapper)
+    mapper_limits.on_source_change(attr, old, new)
+    assert color_mapper.low == 0
+    assert color_mapper.high == 8
