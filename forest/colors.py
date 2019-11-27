cb679b36d5442b4922838f026442c604f8fef58a
afinnen <56884845+afinnen@users.noreply.github.com>
2019-11-27 16:38:31 +0000

    Deleted old Invisibility Class

--- forest/colors.py
+++ forest/colors.py
@@ -281,43 +281,6 @@ class UserLimits(Observable):
             self.inputs["low"].value = str(props["low"])
 
 
-class Invisible:
-    """Control transparency thresholds"""
-    def __init__(self, color_mapper):
-        self.color_mapper = color_mapper
-        self.invisible_on = False
-        self.low = 0
-        self.invisible_checkbox = bokeh.models.CheckboxButtonGroup(
-            labels=["Invisible"],
-            active=[])
-        self.invisible_checkbox.on_change("active",
-                self.on_invisible_checkbox)
-        self.invisible_input = bokeh.models.TextInput(
-                title="Low:",
-                value="0")
-        self.invisible_input.on_change("value",
-                self.on_invisible_input)
-        self.layout = bokeh.layouts.column(
-                self.invisible_checkbox,
-                self.invisible_input)
-
-    def on_invisible_checkbox(self, attr, old, new):
-        if len(new) == 1:
-            self.invisible_on = True
-        else:
-            self.invisible_on = False
-
-    def on_invisible_input(self, attr, old, new):
-        self.low = float(new)
-
-    def render(self):
-        if self.invisible_on:
-            low = self.low
-            color = bokeh.colors.RGB(0, 0, 0, a=0)
-            self.color_mapper.low_color = color
-            self.color_mapper.low = low
-
-
 def state_to_props(state):
     """Map state to props relevant to component"""
     return state.get("colorbar", None)
