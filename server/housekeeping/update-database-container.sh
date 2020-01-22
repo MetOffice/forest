14ed072b5b99da5901d516eeac717f5df1b00a37
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-01-22 11:58:58 +0000

    add a setup.py task inside container to use forestdb

--- server/housekeeping/update-database-container.sh
+++ server/housekeeping/update-database-container.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -x
+
+# Make a custom directory inside container
+PREFIX=/home/custom
+mkdir -p ${PREFIX}
+
+# Install forestdb to custom directory needed by custom Python
+cd /repo/forest
+PYTHONPATH=${PREFIX}/lib/python3.6/site-packages \
+    python setup.py install --prefix ${PREFIX}
+cd -
+
+# Check forestdb available inside container
+PYTHONPATH=${PREFIX}/lib/python3.6/site-packages ${PREFIX}/bin/forestdb -h
+
+# Run custom update Python script
 ls -l /database
 ls -l /repo/forest
 ls -l /s3
