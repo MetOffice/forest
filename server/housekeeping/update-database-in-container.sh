4491762d3e4f7b9c8d95ca81a4512113dd65dc5f
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-01-22 11:30:34 +0000

    add docker run command to crontab

---
+++ server/housekeeping/update-database-in-container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+ls -l /database
+ls -l /repo/forest
+ls -l /s3
--- server/housekeeping/update-database.sh
+++ server/housekeeping/update-database.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Script to keep database in-sync with S3 contents
-echo "Hello, World!"
+# Script to keep local database in-sync with S3 contents
+docker run \
+    --rm \
+    --name forest-cron \
+    -v /home/ec2-user/forest:/repo/forest \
+    -v /home/ec2-user/database:/database \
+    -v /s3/met-office-rmed-forest:/s3/met-office-rmed-forest \
+    informaticslab/forest bash -c '. /repo/forest/server/housekeeping/update-database-in-container.sh'
