add76b3c782f982c71bef8f90861c81d5946591b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-01-22 13:33:23 +0000

    add a simple checker to find files on disk not already in database

--- server/housekeeping/update-database.py
+++ server/housekeeping/update-database.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
+"""
+Housekeeping task to keep SQL database up-to-date
+"""
+import argparse
+import os
+import sqlite3
+import glob
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bucket_dir")
+    parser.add_argument("database_file")
+    return parser.parse_args()
 
 
 def main():
-    print("Hello, World!")
+    args = parse_args()
+
+    # SQL database contents
+    query = "SELECT name FROM file WHERE name GLOB ?;"
+    sql_glob_pattern = "*vietnam*"
+    connection = sqlite3.connect(args.database_file)
+    cursor = connection.cursor()
+    rows = cursor.execute(query, sql_glob_pattern).fetchall()
+    sql_names = [os.path.basename(row[0]) for row in rows]
+    connection.close()
+
+    # S3 bucket contents
+    pattern = os.path.join(args.bucket_dir, "wcssp/*vietnam*.nc")
+    paths = glob.glob(pattern)
+    s3_names = [os.path.basename(path) for path in paths]
+
+    # Summary
+    extra_names = set(s3_names) - set(sql_names)
+    for name in sorted(extra_names):
+        print(name)
 
 
 if __name__ == '__main__':
