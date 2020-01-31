401647129921df346cc0b2e3193353796f97dc8f
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-01-31 16:05:41 +0000

    use parametrize decorator

--- test/test_key_press.py
+++ test/test_key_press.py
@@ -1,5 +1,7 @@
+import pytest
 import forest
 import unittest.mock
+from forest.db import next_value, previous_value
 
 
 def test_on_change_emits_action():
@@ -12,23 +14,13 @@ def test_on_change_emits_action():
     listener.assert_called_once_with(action)
 
 
-def test_navigate_maps_arrow_right_to_next_valid_time():
-    check_key("ArrowRight", forest.db.next_value("valid_time", "valid_times"))
-
-
-def test_navigate_maps_arrow_left_to_previous_valid_time():
-    check_key("ArrowLeft", forest.db.previous_value("valid_time", "valid_times"))
-
-
-def test_navigate_maps_arrow_up_to_next_initial_time():
-    check_key("ArrowUp", forest.db.next_value("initial_time", "initial_times"))
-
-
-def test_navigate_maps_arrow_down_to_previous_initial_time():
-    check_key("ArrowDown", forest.db.previous_value("initial_time", "initial_times"))
-
-
-def check_key(code, action):
+@pytest.mark.parametrize("code,action", [
+    ("ArrowRight", next_value("valid_time", "valid_times")),
+    ("ArrowLeft", previous_value("valid_time", "valid_times")),
+    ("ArrowUp", next_value("initial_time", "initial_times")),
+    ("ArrowDown", previous_value("initial_time", "initial_times")),
+])
+def test_key(code, action):
     store = forest.redux.Store(forest.db.reducer)
     actual = list(forest.keys.navigate(store, forest.keys.press(code)))
     expected = [action]
