49680775869af7181f689bb9e9fd26c2d48822c1
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-02-08 09:42:26 +0000

    use simple html nav, footer and side-nav to clean up look/feel

---
+++ forest/static/script.js
@@ -0,0 +1,8 @@
+let openSideNav = function() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "251px";
+}
+let closeSideNav = function() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
