02e5f5eaa1411625be411caf10de1b6be04763d6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-03-27 16:59:54 +0000

    minimal example of working Argo driver

--- forest/drivers/argo.py
+++ forest/drivers/argo.py
@@ -1,17 +1,19 @@
-import unittest.mock
+import bokeh.models
+import netCDF4
+import forest.geo
 
 class Dataset:
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, pattern=None, **kwargs):
+        self.pattern = pattern
 
     def navigator(self):
         return Navigator()
 
     def map_view(self):
-        return View()
+        return MapView(self.pattern)
 
     def profile_view(self, figure):
-        return ProfileView()
+        return ProfileView(figure)
 
 
 class Navigator:
@@ -28,14 +30,49 @@ class Navigator:
         return []
 
 
-class View:
-    def add_figure(self, *args, **kwargs):
-        return unittest.mock.Mock()
+class MapView:
+    def __init__(self, path):
+        self.path = path
+        with netCDF4.Dataset(self.path) as dataset:
+            self.lons = dataset.variables["LONGITUDE"][:]
+            self.lats = dataset.variables["LATITUDE"][:]
+        self.source = bokeh.models.ColumnDataSource({
+            "x": [],
+            "y": [],
+        })
+
+    def add_figure(self, figure):
+        return figure.circle(x="x", y="y", source=self.source)
 
     def render(self, *args, **kwargs):
-        pass
+        x, y = forest.geo.web_mercator(self.lons, self.lats)
+        print(x, y)
+        self.source.data = {
+            "x": x,
+            "y": y,
+        }
 
 
 class ProfileView:
+    def __init__(self, figure):
+        self.figure = figure
+        self.source = bokeh.models.ColumnDataSource({
+            "x": [],
+            "y": [],
+        })
+        self.figure.circle(x="x", y="y", source=self.source)
+
     def connect(self, store):
-        print("Hello, World!")
+        store.add_subscriber(self.render)
+
+    def render(self, state):
+        if "position" in state:
+            x = state["position"]["x"]
+            y = state["position"]["y"]
+            lons, lats = forest.geo.plate_carree([x], [y])
+
+            # Read data from file??
+            self.source.stream({
+                "x": lons,
+                "y": lats,
+            })
