861961bf0b09ba2fe903574487063588d5553d53
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-04-09 13:03:18 +0100

    add UI to specify dataset/variable combinations

--- forest/components/modal.py
+++ forest/components/modal.py
@@ -10,7 +10,22 @@ class Modal(Observable):
         div = bokeh.models.Div(text="Add layer",
                                css_classes=["custom"],
                                sizing_mode="stretch_width")
-        self.select = bokeh.models.Select(options=[])
+        self.inputs = {}
+
+        # Text-input to name layer
+        self.inputs["name"] = bokeh.models.TextInput(
+            title="Name:",
+            placeholder="Enter text")
+
+        # Select layer settings, e.g. dataset, variable, etc.
+        self.selects = {}
+        self.selects["dataset"] = bokeh.models.Select(
+            title="Dataset:",
+            options=[])
+        self.selects["variable"] = bokeh.models.Select(
+            title="Variable:",
+            options=[])
+
         buttons = (
             bokeh.models.Button(label="Save"),
             bokeh.models.Button(label="Exit"))
@@ -23,7 +38,9 @@ class Modal(Observable):
             button.js_on_click(custom_js)
         self.layout = bokeh.layouts.column(
             div,
-            self.select,
+            self.inputs["name"],
+            self.selects["dataset"],
+            self.selects["variable"],
             bokeh.layouts.row(*buttons, sizing_mode="stretch_width"),
             name="modal",
             sizing_mode="stretch_width")
@@ -35,15 +52,19 @@ class Modal(Observable):
         return self
 
     def render(self, state):
-        self.select.options = self.to_props(state)
-        if len(self.select.options) > 0:
-            if self.select.value == "":
-                self.select.value = self.select.options[0]
+        # Set name for layer, e.g. layer-0
+        self.inputs["name"].value = "layer-0"
+
+        # Configure available datasets
+        self.selects["dataset"].options = self.to_props(state)
+        if len(self.selects["dataset"].options) > 0:
+            if self.selects["dataset"].value == "":
+                self.selects["dataset"].value = self.selects["dataset"].options[0]
 
     def to_props(self, state):
         return [name for name, _ in state.get("patterns", [])]
 
     def on_save(self):
-        name = self.select.value
+        name = self.inputs["name"].value
         action = layers.add_layer(name)
         self.notify(action)
