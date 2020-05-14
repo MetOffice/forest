c182d53c8dd9f9143ac22bd65a0bae3726dace9b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-05-14 13:08:50 +0100

    add support for EarthNetworks images

--- test/test_earth_networks.py
+++ test/test_earth_networks.py
@@ -1,3 +1,6 @@
+import pytest
+import pandas as pd
+import pandas.testing as pdt
 import datetime as dt
 import numpy as np
 import glob
@@ -16,8 +19,8 @@ def test_earth_networks(tmpdir):
     with open(path, "w") as stream:
         stream.writelines(LINES)
 
-    loader = earth_networks.Loader([path])
-    frame = loader.load_date(dt.datetime(2019, 4, 17))
+    loader = earth_networks.Loader()
+    frame = loader.load([path])
     result = frame.iloc[0]
     atol = 0.000001
     if isinstance(result["date"], dt.datetime):
@@ -36,11 +39,20 @@ def test_dataset():
     assert isinstance(dataset, forest.drivers.earth_networks.Dataset)
 
 
-def test_dataset_navigator():
-    settings = {
-        "pattern": "*.txt"
-    }
+def get_navigator(settings):
     dataset = forest.drivers.get_dataset("earth_networks", settings)
-    navigator = dataset.navigator()
-    assert isinstance(navigator,
-                      forest.drivers.earth_networks.Navigator)
+    return dataset.navigator()
+
+
+def test_dataset_navigator():
+    navigator = get_navigator({"pattern": "*.txt"})
+    assert isinstance(navigator, forest.drivers.earth_networks.Navigator)
+
+
+def test_navigator_variables():
+    navigator = earth_networks.Navigator([])
+    assert navigator.variables(None) == [
+        "Cloud-ground strike density",
+        "Intra-cloud strike density",
+        "Time since recent flash"
+    ]
