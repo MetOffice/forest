18afc7b79b71ecb6711c9b81b65d7605c56943e7
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-05-14 10:37:50 +0100

    add a custom build step to run npm

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 """
 import os
 import re
+import subprocess
 import setuptools
+import setuptools.command.build_py
+import setuptools.command.develop
+import setuptools.command.install
 
 
 NAME = "forest"
@@ -27,11 +31,57 @@ def load(fname):
     return result
 
 
+def build_js(command_subclass):
+    """Decorator to call npm install and npm run build"""
+    subclass_run = command_subclass.run
+    def run(self):
+        self.run_command("build_js")
+        subclass_run(self)
+    command_subclass.run = run
+    return command_subclass
+
+
+@build_js
+class InstallCommand(setuptools.command.install.install):
+    """Python and JS code"""
+
+
+@build_js
+class DevelopCommand(setuptools.command.develop.develop):
+    """Python and JS code"""
+
+
+@build_js
+class BuildPyCommand(setuptools.command.build_py.build_py):
+    """Python and JS code"""
+
+
+class BuildJSCommand(setuptools.command.build_py.build_py):
+    """Use nodejs and npm commands to browserify forest.js
+
+    .. note:: Assume current working directory is package ROOT
+    """
+    def run(self):
+        cwd = os.getcwd()
+        os.chdir("forest/js")
+        if not os.path.exists("node_modules"):
+            subprocess.check_call(["npm", "install"])
+        subprocess.check_call(["npm", "run", "build"])
+        os.chdir(cwd)
+        super().run()
+
+
 setuptools.setup(
         name=NAME,
         version=find_version(),
         author="Andrew Ryan",
         author_email="andrew.ryan@metoffice.gov.uk",
+        cmdclass={
+            "install": InstallCommand,
+            "develop": DevelopCommand,
+            "build_py": BuildPyCommand,
+            "build_js": BuildJSCommand,
+        },
         description="Forecast visualisation and survey tool",
         packages=setuptools.find_packages(),
         package_data={
