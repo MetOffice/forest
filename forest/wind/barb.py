5c937cc96e7a90dbe38f4f19ab3928991d563543
Dan Walker <daniel.walker@ncas.ac.uk>
2020-05-19 09:28:21 +0100

    Update windbarb to Bokeh 2.0 and decorators

--- forest/wind/barb.py
+++ forest/wind/barb.py
@@ -1,8 +1,6 @@
 import bokeh.plotting
-from bokeh.plotting.helpers import _glyph_function
 import bokeh.models
 from bokeh.core.properties import DistanceSpec
-
 
 class Barb(bokeh.models.XYGlyph):
     __implementation__ = "barb.ts"
@@ -12,7 +10,11 @@ class Barb(bokeh.models.XYGlyph):
     u = DistanceSpec(units_default="screen")
     v = DistanceSpec(units_default="screen")
 
+def barb():
+    '''Dummy function to be replaced with decorated function'''
+    return True
+
 
 # Extend bokeh.plotting.Figure to support .barb()
 if not hasattr(bokeh.plotting.Figure, 'barb'):
-    bokeh.plotting.Figure.barb = _glyph_function(Barb)
+    bokeh.plotting.Figure.barb = bokeh.plotting._decorators.glyph_method(Barb)(barb)
