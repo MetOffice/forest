b57de0844b54d2ac99734e5dc216c236112041f4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-05-29 11:56:14 +0100

    add stub NavigationService

---
+++ forest/services.py
@@ -0,0 +1,9 @@
+"""Application wide services to de-couple components"""
+
+
+class NavigationService:
+    def method(self):
+        """Callable"""
+
+
+navigation = NavigationService()  # TODO: Find a better place to configure this
---
+++ test/test_services_navigation.py
@@ -0,0 +1,6 @@
+import forest.services
+
+
+def test_navigation_service():
+    """experimental API"""
+    forest.services.navigation.method()
