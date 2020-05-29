f54a0ac483247c2927a8ac90e05b43da8cf509e3
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-05-29 14:42:56 +0100

    further fleshed out service locator pattern

--- forest/services.py
+++ forest/services.py
@@ -3,14 +3,41 @@
 
 class NullNavigator:
     """Navigator API"""
-    def valid_times(self):
-        pass
+    def variables(self, pattern):
+        return []
+
+    def initial_times(self, pattern, variable):
+        return []
+
+    def valid_times(self, pattern, variable, initial_time):
+        return []
+
+    def pressures(self, pattern, variable, initial_time):
+        return []
 
 
-class NavigatorLocator:
+class NavigatorServiceLocator:
+    """De-couples client-code from Navigator construction
+
+    Configured at run-time by calling ``add_dataset(key, dataset)``
+    """
+    def __init__(self):
+        self.datasets = {}
+
+    def add_dataset(self, name, dataset):
+        """Register Dataset(s) with service at run-time"""
+        self.datasets[name] = dataset
+
     def get_navigator(self, dataset_name):
-        """Find appropriate Navigator"""
-        return NullNavigator()
+        """Find appropriate Navigator
+
+        .. note:: If service unavailable a NullNavigator is returned to
+                  prevent system crash
+        """
+        try:
+            return self.datasets[dataset_name].navigator()
+        except KeyError:
+            return NullNavigator()
 
 
-navigation = NavigatorLocator()  # TODO: Find a better place to configure this
+navigation = NavigatorServiceLocator()  # TODO: Find a better place to configure this
