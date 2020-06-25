97eaf4b4b118c70c7fbd5700189a05a42cb64b0b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-06-25 10:28:33 +0100

    add cross-browser fullscreen mode

--- forest/static/script.js
+++ forest/static/script.js
@@ -7,3 +7,40 @@ let openId = function(id, width) {
 let closeId = function(id) {
     document.getElementById(id).style.width = "0";
 }
+
+// Cross-browser full screen
+function openFullscreen(el) {
+    if (el.requestFullscreen) {
+        el.requestFullscreen()
+    } else if (el.mozRequestFullScreen) {
+        /* Mozilla */
+        el.mozRequestFullScreen()
+    } else if (el.webkitRequestFullscreen) {
+        /* Chrome, Safari, Opera */
+        el.webkitRequestFullscreen()
+    } else if (el.msRequestFullscreen) {
+        /* IE/Edge */
+        el.msRequestFullscreen()
+    }
+}
+
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+
+function getFullscreenElement() {
+    return document.fullscreenElement || /* Standard syntax */
+        document.webkitFullscreenElement || /* Chrome, Safari and Opera syntax */
+        document.mozFullScreenElement ||/* Firefox syntax */
+        document.msFullscreenElement /* IE/Edge syntax */
+}
