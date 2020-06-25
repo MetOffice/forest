6860e34fe5041594a22e7681e4095e4b1a7d7e68
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-06-25 09:57:51 +0100

    tweak layout and button stylings

--- forest/static/script.js
+++ forest/static/script.js
@@ -1,5 +1,8 @@
-let openId = function(id) {
-    document.getElementById(id).style.width = "400px";
+let openId = function(id, width) {
+    if (typeof width === "undefined") {
+        width = "400px";
+    }
+    document.getElementById(id).style.width = width;
 }
 let closeId = function(id) {
     document.getElementById(id).style.width = "0";
