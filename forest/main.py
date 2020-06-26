9ccf8ac71baf4409e686e9f66cc5ddb4dd17df30
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-06-26 18:02:46 +0100

    Add T+N to the Title div

--- forest/main.py
+++ forest/main.py
@@ -398,8 +398,14 @@ class Title:
     def render(self, state):
         if isinstance(state, dict):
             state = forest.state.State.from_dict(state)
+        text = ""
         if isinstance(state.valid_time, dt.datetime):
-            self.div.text = f"{state.valid_time:%A %d %B %Y %H:%M}"
+            text = f"{state.valid_time:%A %d %B %Y %H:%M}"
+            if isinstance(state.initial_time, dt.datetime):
+                elapsed = state.valid_time - state.initial_time
+                hours = int((elapsed).total_seconds() / 3600)
+                text = f"{text} T{hours:+}"
+        self.div.text = text
 
 
 class Navbar:
