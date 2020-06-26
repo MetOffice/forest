d0e5e907fe69970b876cd27e952bad0f6d024262
twsearle <14909402+twsearle@users.noreply.github.com>
2020-06-26 16:44:01 +0100

    test ProfileView construction

--- test/test_drivers_argo.py
+++ test/test_drivers_argo.py
@@ -2,7 +2,17 @@ import forest.drivers
 import bokeh.plotting
 
 
-def test_argo_dataset():
+# I believe this ought to be a pytest fixture but I haven't learnt how to use
+# those yet
+def _argo_dataset_setup():
     figure = bokeh.plotting.figure()
     dataset = forest.drivers.get_dataset("argo", {})
-    dataset.profile_view(figure)
+    return figure, dataset
+
+
+def test_argo_profile_view():
+    figure, dataset = _argo_dataset_setup()
+    dataset.paths=[None,]
+    profile_view = dataset.profile_view(figure)
+    state = {}
+    profile_view.render(state)
