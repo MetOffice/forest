2e45e394e5bd83bda6a8c43c71743cbc3eb008a9
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-02-23 16:15:54 +0000

    add support for a label and tooltips

--- forest/drivers/xarray_forecast.py
+++ forest/drivers/xarray_forecast.py
@@ -22,7 +22,8 @@ def no_args_kwargs(method):
 class Dataset:
     """Facade to map framework to xarray dataset"""
 
-    def __init__(self, path, engine):
+    def __init__(self, path, engine, label=""):
+        self.label = label
         self.xarray_dataset = xarray.open_dataset(path, engine=engine)
 
     def navigator(self):
@@ -34,14 +35,15 @@ class Dataset:
 
     def image_loader(self):
         """Construct ImageLoader"""
-        return ImageLoader(self.xarray_dataset)
+        return ImageLoader(self.xarray_dataset, label=self.label)
 
 
 class ImageLoader:
     """Fetch data suitable for bokeh.models.Image glyph"""
 
-    def __init__(self, xarray_dataset):
+    def __init__(self, xarray_dataset, label=""):
         self.xarray_dataset = xarray_dataset
+        self.label = label
 
     def image(self, state):
         forecast_reference_time = to_datetime(state.initial_time)
@@ -50,21 +52,48 @@ class ImageLoader:
         try:
             data_array = data_array.sel(
                 forecast_reference_time=forecast_reference_time,
-                forecast_period=forecast_period
+                forecast_period=forecast_period,
             )
             print(data_array)
         except KeyError as error:
             print(f"Caught KeyError: {error}")
-            return self.empty_image()
+            return self.empty()
         x = np.ma.masked_invalid(data_array.longitude)[:]
         y = np.ma.masked_invalid(data_array.latitude)[:]
         z = np.ma.masked_invalid(data_array)[:]
         if z.mask.all():
-            return self.empty_image()
+            return self.empty()
         else:
             bokeh_data = forest.geo.stretch_image(x, y, z)
+            bokeh_data.update(
+                self.metadata(
+                    self.label,
+                    getattr(data_array, "units", ""),
+                    forecast_reference_time,
+                    forecast_period,
+                )
+            )
             print(bokeh_data)
             return bokeh_data
+
+    @staticmethod
+    def metadata(name, units, forecast_reference_time, forecast_period):
+        hours = (forecast_period).total_seconds() / (60 * 60)
+        length = "T{:+}".format(int(hours))
+        return {
+            "name": [name],
+            "units": [units],
+            "valid": [forecast_reference_time + forecast_period],
+            "initial": [forecast_reference_time],
+            "length": [length],
+            "level": [1.5],
+        }
+
+    def empty(self):
+        """Default bokeh column data source structure"""
+        image = self.empty_image()
+        image.update(self.empty_metadata())
+        return image
 
     @staticmethod
     def empty_image():
@@ -74,6 +103,17 @@ class ImageLoader:
             "dw": [],
             "dh": [],
             "image": [],
+        }
+
+    @staticmethod
+    def empty_metadata():
+        return {
+            "length": [],
+            "initial": [],
+            "valid": [],
+            "level": [],
+            "units": [],
+            "name": [],
         }
 
 
@@ -93,7 +133,9 @@ class Navigator:
 
     def valid_times(self, pattern, variable, initial_time):
         """List of validity times from a particular model run"""
-        forecast_reference_time = self.xarray_dataset.forecast_reference_time.sel(forecast_reference_time=initial_time)
+        forecast_reference_time = self.xarray_dataset.forecast_reference_time.sel(
+            forecast_reference_time=initial_time
+        )
         forecast_period = self.xarray_dataset.forecast_period
         return (forecast_reference_time + forecast_period).to_dict()["data"]
 
